eabb7fa16971405f970a8a59ab66e04997726108
JacopoPatroclo <jacopo.patroclo.martinelli@gmail.com>
2024-02-23 23:22:22 +0100

    doc: update jsdocs for jsx and jsxs functions

--- jsx-runtime.d.ts
+++ jsx-runtime.d.ts
@@ -1,9 +1,13 @@
 import type { Children, Fragment } from './index';
 
 /**
- * Generates a html string from the given contents.
+ * Generates a html string from an attribute name of component and it's props. This
+ * function is meant to be used by the jsx runtime and should not be called directly. This
+ * function only expexts a single child element.
  *
- * // TODO: JSDOC
+ * @param name The name of the element to create or another component function
+ * @param attributes The props to apply to the component
+ * @retuns The generated html string or a promise that resolves to the generated html string
  */
 export function jsx(
   this: void,
@@ -12,9 +16,13 @@ export function jsx(
 ): JSX.Element;
 
 /**
- * Generates a html string from the given contents.
+ * Generates a html string from an attribute name of component and it's props. This
+ * function is meant to be used by the jsx runtime and should not be called directly. This
+ * function only expexts multiple children.
  *
- * // TODO: JSDOC
+ * @param name The name of the element to create or another component function
+ * @param attributes The props to apply to the component
+ * @retuns The generated html string or a promise that resolves to the generated html string
  */
 export function jsxs(
   this: void,
