ad0f79fd9ee9a3969ff74f8a47dcf9430734e405
Arthur Fiorette <me@arthur.place>
2024-02-29 18:21:21 -0300

    initial monorepo with changesets

--- benchmarks/runner/index.js
+++ benchmarks/runner/index.js
@@ -2,55 +2,16 @@ process.env.NODE_ENV = 'production';
 
 import { bench, group, run } from 'mitata';
 
-import KitaHtmlJSXRuntimeRenderers from 'benchmark-kitajs';
-import ReactJSXRuntimeRenderers from 'benchmark-react';
-import StringTemplateRenderers from 'benchmark-templates';
-import TypedHtmlRenderers from 'benchmark-typed-html';
+import KitaHtmlJSXRuntimeRenderers from '@kitajs/bench-html-kitajs';
+import ReactJSXRuntimeRenderers from '@kitajs/bench-html-reactjsx';
+import StringTemplateRenderers from '@kitajs/bench-html-templates';
+import TypedHtmlRenderers from '@kitajs/bench-html-typed-html';
 
 import CommonTags from 'common-tags';
-import * as gHtml from 'ghtml';
 import ReactDOMServer from 'react-dom/server';
+import { assertHtml } from './assertions.js';
 
-import assert from 'node:assert';
-
-// ENSURE THAT ALL RENDERERS PRODUCE THE SAME OUTPUT AGAINST KITAJS/HTML
-// Ensures that Kitajs/html and react produce the same output
-assert.equal(
-  KitaHtmlJSXRuntimeRenderers.ManyComponents('Hello World!'),
-  ReactDOMServer.renderToStaticMarkup(
-    ReactJSXRuntimeRenderers.ManyComponents('Hello World!')
-  )
-);
-
-// Ensures that Kitajs/html and common-tags produce the same output
-assert.equal(
-  KitaHtmlJSXRuntimeRenderers.ManyComponents('Hello World!'),
-  // Simply removes spaces and newlines
-  StringTemplateRenderers.TemplateManyComponents(CommonTags.html, 'Hello World!')
-    .split('\n')
-    .map((l) => l.trim())
-    .join('')
-);
-
-// Ensures that Kitajs/html and ghtml produce the same output
-assert.equal(
-  KitaHtmlJSXRuntimeRenderers.ManyComponents('Hello World!'),
-  // Simply removes spaces and newlines
-  StringTemplateRenderers.TemplateManyComponents(gHtml.html, 'Hello World!')
-    .split('\n')
-    .map((l) => l.trim())
-    .join('')
-);
-
-// Kitajs/html and typed html does produces the same output, however typed-html appends spaces between tags
-assert.equal(
-  KitaHtmlJSXRuntimeRenderers.ManyComponents('Hello World!'),
-  // Simply removes spaces and newlines
-  TypedHtmlRenderers.ManyComponents('Hello World!')
-    .toString()
-    .replace(/< \//g, '</')
-    .replace(/\n/g, '')
-);
+assertHtml();
 
 // EXECUTE THE BENCHMARKS
 group('Many Components (31.4kb)', () => {
@@ -64,9 +25,9 @@ group('Many Components (31.4kb)', () => {
   bench('Common Tags', () =>
     StringTemplateRenderers.TemplateManyComponents(CommonTags.html, 'Hello World!')
   );
-  bench('Ghtml', () =>
-    StringTemplateRenderers.TemplateManyComponents(gHtml.html, 'Hello World!')
-  );
+  // bench('Ghtml', () =>
+  //   StringTemplateRenderers.TemplateManyComponents(gHtml.html, 'Hello World!')
+  // );
 });
 
 group('Many Props (7.4kb)', () => {
@@ -80,9 +41,9 @@ group('Many Props (7.4kb)', () => {
   bench('Common Tags', () =>
     StringTemplateRenderers.TemplateManyProps(CommonTags.html, 'Hello World!')
   );
-  bench('Ghtml', () =>
-    StringTemplateRenderers.TemplateManyProps(gHtml.html, 'Hello World!')
-  );
+  // bench('Ghtml', () =>
+  //   StringTemplateRenderers.TemplateManyProps(gHtml.html, 'Hello World!')
+  // );
 });
 
 group('MdnHomepage (66.7Kb)', () => {
@@ -96,9 +57,9 @@ group('MdnHomepage (66.7Kb)', () => {
   bench('Common Tags', () =>
     StringTemplateRenderers.TemplateMdnHomepage(CommonTags.html, 'Hello World!')
   );
-  bench('Ghtml', () =>
-    StringTemplateRenderers.TemplateMdnHomepage(gHtml.html, 'Hello World!')
-  );
+  // bench('Ghtml', () =>
+  //   StringTemplateRenderers.TemplateMdnHomepage(gHtml.html, 'Hello World!')
+  // );
 });
 
 run().catch(console.error);
