c8b59d897511e9ec4b3c12299b73e80c22a402d2
Arthur Fiorette <me@arthur.place>
2024-05-26 22:34:04 -0300

    docs: errors links

--- packages/ts-html-plugin/src/errors.ts
+++ packages/ts-html-plugin/src/errors.ts
@@ -1,22 +1,22 @@
 export const Xss = {
   code: '0 K601' as any,
   message:
-    'Usage of xss-prone content without `safe` attribute. https://kitajs.github.io/ts-html-plugin#k601'
+    'Usage of xss-prone content without `safe` attribute. https://kita.js.org/html/guides/errors#k601'
 };
 
 export const DoubleEscape = {
   code: '0 K602' as any,
   message:
-    'Double escaping detected. Please remove the `safe` attribute. https://kitajs.github.io/ts-html-plugin#k602'
+    'Double escaping detected. Please remove the `safe` attribute. https://kita.js.org/html/guides/errors#k602'
 };
 
 export const ComponentXss = {
   code: '0 K603' as any,
   message:
-    'Xss-prone content inside a Component, wrap it into a Html.escapeHtml() call. https://kitajs.github.io/ts-html-plugin#k603'
+    'Xss-prone content inside a Component, wrap it into a Html.escapeHtml() call. https://kita.js.org/html/guides/errors#k603'
 };
 
 export const UnusedSafe = {
   code: '0 K604' as any,
-  message: 'Unused safe attribute. https://kitajs.github.io/ts-html-plugin#k604'
+  message: 'Unused safe attribute. https://kita.js.org/html/guides/errors#k604'
 };
