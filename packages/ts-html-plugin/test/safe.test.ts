fbe1d967e9a16620af2e73be47a66eb779fa13fd
Duod-Notyap <paytondoud@gmail.com>
2025-07-25 15:04:16 -0700

    add tests

--- packages/ts-html-plugin/test/safe.test.ts
+++ packages/ts-html-plugin/test/safe.test.ts
@@ -6,6 +6,16 @@ it('Allow correct xss usage', async () => {
   await using server = new TSLangServer(__dirname);
 
   const diagnostics = await server.openWithDiagnostics/* tsx */ `
+    export function Test() {
+        var email = "unsafeText";
+        return <span safe>{email}</span>;
+    }
+
+    export function Test2() {
+        var eProps = { entryName: "unsafeText" };
+        return <span safe>{eProps.entryName}</span>;
+    }
+
     export default (
       <>
         <div></div>
